7c2ca8cf6de76a8b617112a09451ea1b330fded7
Mark Raynsford <code@io7m.com>
2022-04-18 16:55:57 +0000

    Strengthen specification for array mipmaps
    Affects: https://github.com/io7m/calino/issues/8

--- com.io7m.calino.specification/src/main/coq/Calino/ArrayMipMap.v
+++ com.io7m.calino.specification/src/main/coq/Calino/ArrayMipMap.v
@@ -31,29 +31,6 @@ Inductive arrayMipMapIndicesSorted : list arrayMipMapIndexT → Prop :=
       arrayMipMapIndicesSorted (mmy :: mxs) →
         arrayMipMapIndicesSorted (mmx :: mmy :: mxs).
 
-Example exampleArrayMipMapIndicesSorted : arrayMipMapIndicesSorted [
-  (ArrayMipMapIndex 2 0);
-  (ArrayMipMapIndex 2 1);
-  (ArrayMipMapIndex 2 2);
-  (ArrayMipMapIndex 1 0);
-  (ArrayMipMapIndex 1 1);
-  (ArrayMipMapIndex 1 2);
-  (ArrayMipMapIndex 0 0);
-  (ArrayMipMapIndex 0 1);
-  (ArrayMipMapIndex 0 2)
-].
-Proof.
-  apply AMMICons. { apply AMMIOrdLevelEq. reflexivity. compute; constructor. }
-  apply AMMICons. { apply AMMIOrdLevelEq. reflexivity. compute; constructor. }
-  apply AMMICons. { apply AMIIOrdLevelLt. compute; constructor. }
-  apply AMMICons. { apply AMMIOrdLevelEq. reflexivity. compute; constructor. }
-  apply AMMICons. { apply AMMIOrdLevelEq. reflexivity. compute; constructor. }
-  apply AMMICons. { apply AMIIOrdLevelLt. compute; constructor. }
-  apply AMMICons. { apply AMMIOrdLevelEq. reflexivity. compute; constructor. }
-  apply AMMICons. { apply AMMIOrdLevelEq. reflexivity. compute; constructor. }
-  apply AMMIOne.
-Qed.
-
 Inductive arrayMipMap : Set := ArrayMipMap {
   arrayMipMapIndex            : arrayMipMapIndexT;
   arrayMipMapOffset           : nat;
@@ -69,19 +46,40 @@ Inductive arrayMipMapOffsetsSorted : list arrayMipMap → Prop :=
       arrayMipMapOffsetsSorted (mm0 :: mxs) →
         arrayMipMapOffsetsSorted (mm1 :: mm0 :: mxs).
 
+Fixpoint arrayMipMapLayerCountForLevel
+  (level : nat)
+  (m     : list arrayMipMap)
+: nat :=
+  match m with
+  | []        => 0
+  | (x :: xs) =>
+    match Nat.eq_dec (arrayMipMapLevel (arrayMipMapIndex x)) level with
+    | left _  => 1 + (arrayMipMapLayerCountForLevel level xs)
+    | right _ => (arrayMipMapLayerCountForLevel level xs)
+    end
+  end.
+
+Definition arrayMipMapLevels (m : list arrayMipMap) : list nat :=
+  nodup Nat.eq_dec (map (λ k, arrayMipMapLevel (arrayMipMapIndex k)) m).
+
+Definition arrayMipMapsHaveSameLayers : (list arrayMipMap) → nat → nat → Prop :=
+  λ m level0 level1, 
+    In level0 (arrayMipMapLevels m) →
+      In level1 (arrayMipMapLevels m) →
+        arrayMipMapLayerCountForLevel level0 m = arrayMipMapLayerCountForLevel level1 m.
+
 Inductive arrayMipMapList : Set := ArrayMipMapList {
   arrayMipMaps                : list arrayMipMap;
   arrayMipMapIndicesAreSorted : arrayMipMapIndicesSorted (map arrayMipMapIndex arrayMipMaps);
   arrayMipMapOffsetAreSorted  : arrayMipMapOffsetsSorted arrayMipMaps;
+  arrayMipMapSameLayers       : ∀ level0 level1, arrayMipMapsHaveSameLayers arrayMipMaps level0 level1
 }.
 
 Inductive arrayMipMapImageSize : Set := ArrayMipMapImageSize {
-  ammSizeX           : nat;
-  ammSizeY           : nat;
-  ammSizeLayers      : nat;
-  ammSizeXRange      : 1 < ammSizeX;
-  ammSizeYRange      : 1 < ammSizeY;
-  ammSizeLayersRange : 0 < ammSizeLayers;
+  ammSizeX      : nat;
+  ammSizeY      : nat;
+  ammSizeXRange : 1 < ammSizeX;
+  ammSizeYRange : 1 < ammSizeY;
 }.
 
 Lemma arrayMipMapsNonEmpty : ∀ (m : arrayMipMapList),
@@ -136,10 +134,8 @@ Definition arrayMipMapSize
 : option arrayMipMapImageSize :=
   let sx  := (sizeX imageSize) / 2 ^ level  in
   let sy  := (sizeY imageSize) / 2 ^ level  in
-  let sa  := (sizeZ imageSize) in
-  let sar := lt_neq_0 _ (sizeZnot0 imageSize) in
     match (Nat.ltb_spec0 1 sx, Nat.ltb_spec0 1 sy) with
-    | (ReflectT _ xt, ReflectT _ yt) => Some (ArrayMipMapImageSize sx sy sa xt yt sar)
+    | (ReflectT _ xt, ReflectT _ yt) => Some (ArrayMipMapImageSize sx sy xt yt)
     | (_, _)                         => None
     end.
 
@@ -153,3 +149,99 @@ Fixpoint arrayMipMapImageDataSizeTotalAux (m : list arrayMipMap) : nat :=
 Definition arrayMipMapImageDataSizeTotal (m : arrayMipMapList) : nat :=
   arrayMipMapImageDataSizeTotalAux (arrayMipMaps m).
 
+Example exampleArrayMipMapsIndices := [
+  (ArrayMipMapIndex 2 0);
+  (ArrayMipMapIndex 2 1);
+  (ArrayMipMapIndex 2 2);
+  (ArrayMipMapIndex 1 0);
+  (ArrayMipMapIndex 1 1);
+  (ArrayMipMapIndex 1 2);
+  (ArrayMipMapIndex 0 0);
+  (ArrayMipMapIndex 0 1);
+  (ArrayMipMapIndex 0 2)
+].
+
+Example exampleArrayMipMaps := 
+  map (λ i, ArrayMipMap i 0 0 0 0) exampleArrayMipMapsIndices.
+
+Example exampleArrayMipMapIndicesSorted : arrayMipMapIndicesSorted exampleArrayMipMapsIndices.
+Proof.
+  apply AMMICons. { apply AMMIOrdLevelEq. reflexivity. compute; constructor. }
+  apply AMMICons. { apply AMMIOrdLevelEq. reflexivity. compute; constructor. }
+  apply AMMICons. { apply AMIIOrdLevelLt. compute; constructor. }
+  apply AMMICons. { apply AMMIOrdLevelEq. reflexivity. compute; constructor. }
+  apply AMMICons. { apply AMMIOrdLevelEq. reflexivity. compute; constructor. }
+  apply AMMICons. { apply AMIIOrdLevelLt. compute; constructor. }
+  apply AMMICons. { apply AMMIOrdLevelEq. reflexivity. compute; constructor. }
+  apply AMMICons. { apply AMMIOrdLevelEq. reflexivity. compute; constructor. }
+  apply AMMIOne.
+Qed.
+
+Example exampleArrayMipMapsHaveSameLayers0 : arrayMipMapLayerCountForLevel 0 exampleArrayMipMaps = 3.
+Proof. reflexivity. Qed.
+Example exampleArrayMipMapsHaveSameLayers1 : arrayMipMapLayerCountForLevel 1 exampleArrayMipMaps = 3.
+Proof. reflexivity. Qed.
+Example exampleArrayMipMapsHaveSameLayers2 : arrayMipMapLayerCountForLevel 2 exampleArrayMipMaps = 3.
+Proof. reflexivity. Qed.
+
+Example exampleArrayMipMapsLevel : ∀ n, In n (arrayMipMapLevels exampleArrayMipMaps) →
+  n = 0 \/ n = 1 \/ n = 2.
+Proof.
+  intros n.
+  simpl.
+  intros H.
+  destruct H; auto.
+  destruct H; auto.
+  destruct H; auto.
+  contradiction H.
+Qed.
+
+Example exampleArrayMipMapsHaveSameLayers : ∀ level0 level1, arrayMipMapsHaveSameLayers exampleArrayMipMaps level0 level1.
+Proof.
+  intros level0 level1.
+  unfold arrayMipMapsHaveSameLayers.
+  intros H_In0 H_In1.
+  assert (level0 = 0 ∨ level0 = 1 ∨ level0 = 2) as Hl0 by (apply (exampleArrayMipMapsLevel level0 H_In0)).
+  assert (level1 = 0 ∨ level1 = 1 ∨ level1 = 2) as Hl1 by (apply (exampleArrayMipMapsLevel level1 H_In1)).
+  destruct Hl0 as [Heq0|Heq0]. {
+    destruct Hl1 as [Heq1|Heq1]. {
+      rewrite Heq0.
+      rewrite Heq1.
+      reflexivity.
+    }
+    destruct Heq1 as [Heq1_0|Heq1_0]. {
+      rewrite Heq0.
+      rewrite Heq1_0.
+      reflexivity.
+    }
+    rewrite Heq0.
+    rewrite Heq1_0.
+    reflexivity.
+  }
+  destruct Heq0 as [Heq0_0|Heq0_0]. {
+    rewrite Heq0_0.
+    destruct Hl1 as [Heq1|Heq1]. {
+      rewrite Heq1.
+      reflexivity.
+    }
+    destruct Heq1 as [Heq1_0|Heq1_0]. {
+      rewrite Heq1_0.
+      reflexivity.
+    }
+    rewrite Heq1_0.
+    reflexivity.
+  }
+  destruct Hl1 as [Heq1|Heq1]. {
+    rewrite Heq0_0.
+    rewrite Heq1.
+    reflexivity.
+  }
+  destruct Heq1 as [Heq1_0|Heq1_0]. {
+    rewrite Heq0_0.
+    rewrite Heq1_0.
+    reflexivity.
+  }
+  rewrite Heq0_0.
+  rewrite Heq1_0.
+  reflexivity.
+Qed.
