309cc8f6aa7ea1a9056aa50588682fdcce5dd4f8
Mark Raynsford <code@io7m.com>
2024-10-20 11:03:33 +0000

    Use junit BOM.

--- com.io7m.calino.tests/src/main/java/module-info.java
+++ com.io7m.calino.tests/src/main/java/module-info.java
@@ -37,6 +37,7 @@ open module com.io7m.calino.tests
   requires org.junit.jupiter.engine;
   requires org.junit.platform.commons;
   requires org.junit.platform.engine;
+  requires org.junit.platform.launcher;
 
   requires com.io7m.jmulticlose.core;
   requires com.io7m.wendover.core;
